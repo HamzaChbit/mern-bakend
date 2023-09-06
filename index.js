5861313ad7fee83d49273c46bff04f3f84ec65d0
Hamza Chbit <118445750+HamzaChbit@users.noreply.github.com>
2023-09-06 14:05:47 +0100

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const secret = process.env.SCREET_CODE;
 
 
 
-app.use(cors({origin:['http://localhost:3000','https://mern-stack-chbit-app.onrendre.com'],}));
+app.use(cors({origin:['http://localhost:3000','https://mern-stack-chbit-app.onrendre.com','https://mern-forntend.vercel.app/'],}));
 app.use(express.json())
 app.use(cookieParser())
 app.use('/uploads', express.static(__dirname + '/uploads'));
